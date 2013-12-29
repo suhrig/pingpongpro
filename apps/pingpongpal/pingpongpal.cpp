ece69986b6c14ac8598851907420a549fe8a966f
Sebastian Uhrig <s.uhrig@imb-mainz.de>
2013-12-29 21:15:35 +0100

    segregation of code into functions

--- apps/pingpongpal/pingpongpal.cpp
+++ apps/pingpongpal/pingpongpal.cpp
@@ -1,5 +1,5 @@
 // ==========================================================================
-//                                pingpongpal
+//				pingpongpal
 // ==========================================================================
 
 #include <seqan/basic.h>
@@ -24,10 +24,10 @@ typedef vector<CharString> TInputFiles;
 // struct to store the options from the command line
 struct AppOptions
 {
-        TInputFiles inputFiles;
+	TInputFiles inputFiles;
 
-        AppOptions()
-        {}
+	AppOptions()
+	{}
 };
 
 // constants to refer to + and - strands throughout the program
@@ -40,21 +40,21 @@ const unsigned int STRAND_MINUS = 1;
 //  - readsWithNonTemplateBase: how many of these reads have terminal base which is different from the reference genome (also typical for piRNAs)
 struct TPosition
 {
-        int reads;
-        int readsWithAOrUAtBase10;
-        int readsWithNonTemplateBase;
-        TPosition():
-                reads(0),
-                readsWithAOrUAtBase10(0),
-                readsWithNonTemplateBase(0)
-        {}
+	int reads;
+	int readsWithAOrUAtBase10;
+	int readsWithNonTemplateBase;
+	TPosition():
+		reads(0),
+		readsWithAOrUAtBase10(0),
+		readsWithNonTemplateBase(0)
+	{}
 };
 
-// array of TPosition to store the above stats for each position of a contig/chromosome
-typedef map< unsigned int, TPosition > TPositionMap;
-
-// array of TPositionMap to store the above status for each contig/chromosome
-typedef map< unsigned int, TPositionMap > TContigMap;
+// The following types define nested arrays to store the above stats for every position in the genome.
+// The stats are grouped by strand and contig/chromosome.
+typedef map< unsigned int, TPosition > TContig;
+typedef map< unsigned int, TContig > TStrand;
+typedef TStrand TStrands[2];
 
 // ==========================================================================
 // Functions
@@ -64,152 +64,168 @@ typedef map< unsigned int, TPositionMap > TContigMap;
 // function to parse command-line arguments
 ArgumentParser::ParseResult parseCommandLine(AppOptions & options, int argc, char const ** argv)
 {
-        // Setup ArgumentParser.
-        ArgumentParser parser("pingpongpal");
-        // Set short description, version, and date.
-        setShortDescription(parser, "Put a Short Description Here");
-        setVersion(parser, "0.1");
-        setDate(parser, "July 2012");
+	// Setup ArgumentParser.
+	ArgumentParser parser("pingpongpal");
+	// Set short description, version, and date.
+	setShortDescription(parser, "Put a Short Description Here");
+	setVersion(parser, "0.1");
+	setDate(parser, "July 2012");
 
-        // Define usage line and long description.
-        addUsageLine(parser, "[\\fIOPTIONS\\fP] \"\\fITEXT\\fP\"");
-        addDescription(parser, "This is the application skelleton and you should modify this string.");
+	// Define usage line and long description.
+	addUsageLine(parser, "[\\fIOPTIONS\\fP] \"\\fITEXT\\fP\"");
+	addDescription(parser, "This is the application skelleton and you should modify this string.");
 
-        addOption(parser, ArgParseOption("i", "input", "Path to the input file(s)", ArgParseArgument::INPUTFILE, "IN", true));
-        vector< string > acceptedInputFormats;
-        acceptedInputFormats.push_back(".sam");
-        acceptedInputFormats.push_back(".bam");
-        setValidValues(parser, "input", acceptedInputFormats);
+	addOption(parser, ArgParseOption("i", "input", "Path to the input file(s)", ArgParseArgument::INPUTFILE, "IN", true));
+	vector< string > acceptedInputFormats;
+	acceptedInputFormats.push_back(".sam");
+	acceptedInputFormats.push_back(".bam");
+	setValidValues(parser, "input", acceptedInputFormats);
 
-        // Add Examples Section.
-        addTextSection(parser, "Examples");
-        addListItem(parser, "\\fBpingpongpal\\fP \\fB-v\\fP \\fItext\\fP", "Call with \\fITEXT\\fP set to \"text\" with verbose output.");
+	// Add Examples Section.
+	addTextSection(parser, "Examples");
+	addListItem(parser, "\\fBpingpongpal\\fP \\fB-v\\fP \\fItext\\fP", "Call with \\fITEXT\\fP set to \"text\" with verbose output.");
 
-        // Parse command line.
-        ArgumentParser::ParseResult parserResult = parse(parser, argc, argv);
+	// Parse command line.
+	ArgumentParser::ParseResult parserResult = parse(parser, argc, argv);
 
-        if (parserResult != ArgumentParser::PARSE_OK)
-                return parserResult;
+	if (parserResult != ArgumentParser::PARSE_OK)
+		return parserResult;
 
-        options.inputFiles.resize(getOptionValueCount(parser, "input"));
-        for (vector< string >::size_type i = 0; i < options.inputFiles.size(); i++)
-                getOptionValue(options.inputFiles[i], parser, "input", i);
+	options.inputFiles.resize(getOptionValueCount(parser, "input"));
+	for (vector< string >::size_type i = 0; i < options.inputFiles.size(); i++)
+		getOptionValue(options.inputFiles[i], parser, "input", i);
 
-        return parserResult;
+	return parserResult;
 }
 
 // function to measure time between the first and second invocation of the function
 void tic()
 {
-        static time_t start = 0;
-        if (start != 0)
-        {
-                cout << "Elapsed: " << (time(NULL) - start) << endl;
-                start = 0;
-        }
-        else
-        {
-                start = time(NULL);
-        }
+	static time_t start = 0;
+	if (start != 0)
+	{
+		cout << "Elapsed: " << (time(NULL) - start) << endl;
+		start = 0;
+	}
+	else
+	{
+		start = time(NULL);
+	}
+}
+
+// Function which sums up the number of reads that start at a given position in the genome.
+// Additionally, it counts the number of reads with an A/U at base 10 as well as
+// the number of reads with a terminal base different from the reference genome.
+// The latter two figures help identify the significance of a putative piRNA signature.
+void countReadsInBamFile(BamStream &bamFile, TStrands &readCountsByPosition)
+{
+	TPosition *pposition;
+	size_t seqLength;
+	BamAlignmentRecord record;
+	while (!atEnd(bamFile))
+	{
+		readRecord(record, bamFile);
+		//todo: handle clipped alignments (maybe with getClippedPos?)
+		//todo: count skipped reads
+		if (record.beginPos != BamAlignmentRecord::INVALID_POS)
+		{
+			seqLength = length(record.seq);
+			if (hasFlagRC(record)) {
+				pposition = &(readCountsByPosition[STRAND_MINUS][record.rID][record.beginPos+seqLength]);
+			} else {
+				pposition = &(readCountsByPosition[STRAND_PLUS][record.rID][record.beginPos]);
+			}
+			pposition->reads++;
+			if (seqLength >= 10)
+				//todo: comparison with lowercase sequences
+				if ((record.seq[9] == 'A') || (record.seq[9] == 'T') || (record.seq[9] == 'U'))
+					pposition->readsWithAOrUAtBase10++;
+			pposition->readsWithNonTemplateBase++;
+		}
+	}
+}
+
+// find those positions on the genome, where reads on opposite strands overlap by 10 nucleotides
+void findOverlappingReads(TStrands &readCountsByPosition)
+{
+	/*for (int strand = 0; strand <= 1; strand++)
+	{
+		for(TStrand::iterator contig = readCountsByPosition[strand].begin(); contig != readCountsByPosition[strand].end(); ++contig)
+		{
+			for(TContig::iterator position = contig->second.begin(); position != contig->second.end(); ++position)
+			{
+				cout << strand << "." << contig->first << "." << position->first << ": " << position->second << endl;
+			}
+		}
+	}*/
+
+	int density[1000];
+	for (int i = 0; i < 1000; i++)
+		density[i] = 0;
+	int minCount = 0;
+	TStrand::iterator contigOnOppositeStrand;
+	TContig::iterator positionOnOppositeStrand;
+	for(TStrand::iterator contig = readCountsByPosition[STRAND_PLUS].begin(); contig != readCountsByPosition[STRAND_PLUS].end(); ++contig)
+	{
+		contigOnOppositeStrand = readCountsByPosition[STRAND_MINUS].find(contig->first);
+		if (contigOnOppositeStrand != readCountsByPosition[STRAND_MINUS].end())
+		{
+			for(TContig::iterator position = contig->second.begin(); position != contig->second.end(); ++position)
+			{
+				positionOnOppositeStrand = contigOnOppositeStrand->second.find(position->first + 10);
+				if (positionOnOppositeStrand != contigOnOppositeStrand->second.end())
+				{
+//				      minCount = (position->second.reads < positionOnOppositeStrand->second.reads) ? position->second.reads : positionOnOppositeStrand->second.reads;
+//				      if (minCount > 999)
+//					      minCount = 999;
+//				      density[minCount]++;
+
+//				if ((position->second.reads >= 100) && (positionOnOppositeStrand->second.reads >= 100))
+						cout << contig->first << "." << position->first << ": reads+ " << position->second.reads << ", a/u+ " << position->second.readsWithAOrUAtBase10 << ", non-template bases+ " << position->second.readsWithNonTemplateBase << ", reads- " << positionOnOppositeStrand->second.reads << ", a/u- " << positionOnOppositeStrand->second.readsWithAOrUAtBase10 << ", non-template bases- " << positionOnOppositeStrand->second.readsWithNonTemplateBase << endl;
+
+				}
+			}
+		}
+	}
+
+
+/*      for (int strand = 0; strand <= 1; strand++)
+		for (TStrand::iterator contig = readCountsByPosition[strand].begin(); contig != readCountsByPosition[strand].end(); ++contig)
+			for (TContig::iterator position = contig->second.begin(); position != contig->second.end(); ++position)
+				cout << strand << "." << contig->first << "." << position->first << ": reads " << position->second.reads << ", a/u " << position->second.readsWithAOrUAtBase10 << ", non-template bases " << position->second.readsWithNonTemplateBase << endl;*/
+
+
+//      for (int i = 0; i < 1000; i++)
+//	      cout << i << "\t" << density[i] << endl;
 }
 
 // program entry point
 int main(int argc, char const ** argv)
 {
-        // Parse the command line.
-        AppOptions options;
-        if (parseCommandLine(options, argc, argv) != seqan::ArgumentParser::PARSE_OK)
-                return 1;
+	// Parse the command line.
+	AppOptions options;
+	if (parseCommandLine(options, argc, argv) != ArgumentParser::PARSE_OK)
+		return 1;
 
+	if (options.inputFiles.size() == 0)
+		options.inputFiles.push_back("/dev/stdin");
 
-        TContigMap readCountsByPosition[2];
+	TStrand readCountsByPosition[2];
 
-        if (options.inputFiles.size() == 0)
-                options.inputFiles.push_back("/dev/stdin");
+	// read all BAM/SAM files
+	for(TInputFiles::iterator inputFile = options.inputFiles.begin(); inputFile != options.inputFiles.end(); ++inputFile)
+	{
+		// open SAM/BAM file
+		BamStream bamFile(toCString(*inputFile));
+		// for every position in the genome, count the number of reads that start at a given position
+		countReadsInBamFile(bamFile, readCountsByPosition);
+		// close SAM/BAM file
+		close(bamFile);
+	}
 
-        TPosition *pposition;
-        size_t seqLength;
+	findOverlappingReads(readCountsByPosition);
 
-        for(TInputFiles::iterator inputFile = options.inputFiles.begin(); inputFile != options.inputFiles.end(); ++inputFile)
-        {
-                // Open input stream, BamStream can read SAM and BAM files.
-                BamStream bamFile(toCString(*inputFile));
-
-                BamAlignmentRecord record;
-                while (!atEnd(bamFile))
-                {
-                        readRecord(record, bamFile);
-                        //todo: handle clipped alignments (maybe with getClippedPos?)
-                        //todo: count skipped reads
-                        if (record.beginPos != BamAlignmentRecord::INVALID_POS)
-                        {
-                                seqLength = length(record.seq);
-                                if (hasFlagRC(record)) {
-                                        pposition = &(readCountsByPosition[STRAND_MINUS][record.rID][record.beginPos+seqLength]);
-                                } else {
-                                        pposition = &(readCountsByPosition[STRAND_PLUS][record.rID][record.beginPos]);
-                                }
-                                pposition->reads++;
-                                if (seqLength >= 10)
-                                        //todo: comparison with lowercase sequences
-                                        if ((record.seq[9] == 'A') || (record.seq[9] == 'T') || (record.seq[9] == 'U'))
-                                                pposition->readsWithAOrUAtBase10++;
-                                pposition->readsWithNonTemplateBase++;
-                        }
-                }
-        }
-
-        /*for (int strand = 0; strand <= 1; strand++)
-        {
-                for(TContigMap::iterator contig = readCountsByPosition[strand].begin(); contig != readCountsByPosition[strand].end(); ++contig)
-                {
-                        for(TPositionMap::iterator position = contig->second.begin(); position != contig->second.end(); ++position)
-                        {
-                                cout << strand << "." << contig->first << "." << position->first << ": " << position->second << endl;
-                        }
-                }
-        }*/
-
-        int density[1000];
-        for (int i = 0; i < 1000; i++)
-                density[i] = 0;
-        int minCount = 0;
-        TContigMap::iterator contigOnOppositeStrand;
-        TPositionMap::iterator positionOnOppositeStrand;
-        for(TContigMap::iterator contig = readCountsByPosition[STRAND_PLUS].begin(); contig != readCountsByPosition[STRAND_PLUS].end(); ++contig)
-        {
-                contigOnOppositeStrand = readCountsByPosition[STRAND_MINUS].find(contig->first);
-                if (contigOnOppositeStrand != readCountsByPosition[STRAND_MINUS].end())
-                {
-                        for(TPositionMap::iterator position = contig->second.begin(); position != contig->second.end(); ++position)
-                        {
-                                positionOnOppositeStrand = contigOnOppositeStrand->second.find(position->first + 10);
-                                if (positionOnOppositeStrand != contigOnOppositeStrand->second.end())
-                                {
-//                                      minCount = (position->second.reads < positionOnOppositeStrand->second.reads) ? position->second.reads : positionOnOppositeStrand->second.reads;
-//                                      if (minCount > 999)
-//                                              minCount = 999;
-//                                      density[minCount]++;
-
-                                if ((position->second.reads >= 100) && (positionOnOppositeStrand->second.reads >= 100))
-                                                cout << contig->first << "." << position->first << ": reads+ " << position->second.reads << ", a/u+ " << position->second.readsWithAOrUAtBase10 << ", non-template bases+ " << position->second.readsWithNonTemplateBase << ", reads- " << positionOnOppositeStrand->second.reads << ", a/u- " << positionOnOppositeStrand->second.readsWithAOrUAtBase10 << ", non-template bases- " << positionOnOppositeStrand->second.readsWithNonTemplateBase << endl;
-
-                                }
-                        }
-                }
-        }
-
-
-/*      for (int strand = 0; strand <= 1; strand++)
-                for (TContigMap::iterator contig = readCountsByPosition[strand].begin(); contig != readCountsByPosition[strand].end(); ++contig)
-                        for (TPositionMap::iterator position = contig->second.begin(); position != contig->second.end(); ++position)
-                                cout << strand << "." << contig->first << "." << position->first << ": reads " << position->second.reads << ", a/u " << position->second.readsWithAOrUAtBase10 << ", non-template bases " << position->second.readsWithNonTemplateBase << endl;*/
-
-
-//      for (int i = 0; i < 1000; i++)
-//              cout << i << "\t" << density[i] << endl;
-
-        return 0;
+	return 0;
 }
 
 
